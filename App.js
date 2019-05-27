9ceb006a2543466b6ab7d809e21cfa5cd4ac2eae
Duong Tran <duong.tran@pycogroup.com>
2019-05-27 10:41:19 +0700

    feat: Welcome screen (#1)
    * Removed children components
    
    * Added render child components
    
    * Added css outlines of component style
    
    * Made component tree and highlighted by background color
    
    * Added logo and used Image component to show an image
    
    * Added welcome image
    
    * Updated text's styles
    
    * Styled register button
    
    * Added TouchableOpacity to register button, allow user taps on
    
    * Added fonts
    
    * Added rnpm to link fonts to project,
    
    * Added example font style

--- App.js
+++ App.js
@@ -6,24 +6,75 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View, SafeAreaView, Text, Image, Dimensions, TouchableOpacity } from 'react-native';
+import { FONTS, IMAGES } from './src/assets';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends Component {
 
-type Props = {};
-export default class App extends Component<Props> {
+  renderLogo = () => {
+    return (
+      <Image
+        style={styles.logo}
+        source={IMAGES.logo}
+        resizeMode={'cover'}
+      />
+    );
+  };
+
+  renderWelcomeImage = () => {
+    return (
+      <Image
+        style={styles.welcomeImage}
+        source={IMAGES.welcome.image}
+        resizeMode={'contain'}
+      />
+    )
+  };
+
+  renderWelcomeTitle = () => {
+    return (
+      <Text style={styles.welcomeTitle}>
+        Your ride, on demand
+      </Text>
+    )
+  };
+
+  renderWelcomeText = () => {
+    return (
+      <Text style={styles.welcomeText}>
+        Whether you’re headed to work, the airport, or out on the town, Uber connects you with a reliable ride in
+        minutes. One tap and a car comes directly to you.
+      </Text>
+    );
+  };
+
+  renderRegisterButton = () => {
+    return (
+      <TouchableOpacity>
+      <View style={styles.registerButton}>
+        <Text style={styles.registerTitle}>
+          Register with Phone
+        </Text>
+        <Image
+          source={IMAGES.arrowRight}
+          style={styles.arrowRight}
+        />
+      </View>
+      </TouchableOpacity>
+    );
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <SafeAreaView>
+          {this.renderLogo()}
+          {this.renderWelcomeImage()}
+          {this.renderWelcomeTitle()}
+          {this.renderWelcomeText()}
+          {this.renderRegisterButton()}
+        </SafeAreaView>
       </View>
     );
   }
@@ -32,18 +83,65 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#F5FCFF',
+
+    paddingHorizontal: 30,
+    paddingTop: 37.5,
+    paddingBottom: 43,
+  },
+
+  // logo
+  logo: {
+    width: 137,
+    height: 46,
+    marginBottom: 40
+  },
+
+  // welcome image
+  welcomeImage: {
+    width: Dimensions.get('window').width - 60,
+    height: 350,
+    marginBottom: 40,
+    marginLeft: -20,
+  },
+
+  // welcome title
+  welcomeTitle: {
+    fontSize: 26,
+    marginBottom: 20,
+    lineHeight: 43,
+    fontFamily: FONTS.Oswald.Regular
+  },
+
+  // welcome text,
+  welcomeText: {
+    marginBottom: 40,
+    fontSize: 13.4,
+    lineHeight: 16,
+    fontFamily: FONTS.Oswald.Light
+  },
+
+  // register button
+  registerButton: {
+    height: 50,
+    borderRadius: 5,
+
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'black',
+
+    flexDirection: 'row',
+    paddingHorizontal: 25
   },
-  welcome: {
-    fontSize: 20,
+  registerTitle: {
+    color: 'white',
+    fontSize: 14,
+    flex: 1,
     textAlign: 'center',
-    margin: 10,
+    fontFamily: FONTS.Oswald.Regular
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  arrowRight: {
+    width: 26,
+    height: 21
+  }
 });
---
+++ src/assets/images/index.js
@@ -0,0 +1,9 @@
+const images = {
+  logo: require('./logo.png'),
+  arrowRight: require('./arrow_right.png'),
+  welcome: {
+    image: require('./welcome_image.png')
+  }
+};
+
+export default images;
